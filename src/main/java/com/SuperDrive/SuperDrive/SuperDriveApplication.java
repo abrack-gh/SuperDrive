3e83660ad6028e9180068f84ca757450c658b146
Alex Brackenridge <a.brack@live.co.uk>
2023-05-19 12:45:53 +0100

    Initial commit - Spring Initializr

---
+++ src/main/java/com/SuperDrive/SuperDrive/SuperDriveApplication.java
@@ -0,0 +1,13 @@
+package com.SuperDrive.SuperDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SuperDrive/SuperDrive/SuperDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SuperDrive.SuperDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
